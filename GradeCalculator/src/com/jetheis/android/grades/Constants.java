f407a702c1f3ad0b57f6943ff522777c04f042ba
Jimmy Theis <jimmy@jetheis.com>
2012-04-05 13:55:38 -0400

    Stub out some static stuff
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

---
+++ GradeCalculator/src/com/jetheis/android/grades/Constants.java
@@ -0,0 +1,16 @@
+package com.jetheis.android.grades;
+
+import android.util.Log;
+
+/**
+ * A container for application-wide constants.
+ * 
+ */
+public class Constants {
+
+    /**
+     * The tag to be used with {@link Log} method calls throughout the app.
+     */
+    public static final String TAG = "GradeCalculator";
+
+}
