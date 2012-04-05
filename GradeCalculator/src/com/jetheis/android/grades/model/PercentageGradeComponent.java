3c820221f2fd89335925f267994386c8681a500f
Jimmy Theis <jimmy@jetheis.com>
2012-04-04 21:46:11 -0400

    Add first pass concrete implementations of GradeComponent
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

---
+++ GradeCalculator/src/com/jetheis/android/grades/model/PercentageGradeComponent.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2012 Jimmy Theis. Licensed under the MIT License:
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.jetheis.android.grades.model;
+
+/**
+ * A grade component that is scored based on a percentage of points earned. Its
+ * weight relative to the rest of the course is based on a specific percentage
+ * weight.
+ * 
+ */
+public class PercentageGradeComponent extends GradeComponent {
+
+    private float mEarnedPercentage;
+    private float mWeight;
+
+    /**
+     * Get the percentage of points earned for this grade component. Any actual
+     * point totals are not reflected here; rather, the value tracked is simply
+     * the ratio of points earned to total points.
+     * 
+     * @return The percentage of points earned for this grade component,
+     *         represented as a float less than or equal to 1.0f.
+     */
+    public float getEarnedPercentage() {
+        return mEarnedPercentage;
+    }
+
+    /**
+     * Set the percentage of points earned for this grade component.Any actual
+     * point totals are not reflected here; rather, the value tracked is simply
+     * the ratio of points earned to total points.
+     * 
+     * @param earnedPercentage
+     *            The percentage of points earned for this grade component,
+     *            represented as a float less than or equal to 1.0f.
+     */
+    public void setEarnedPercentage(float earnedPercentage) {
+        mEarnedPercentage = earnedPercentage;
+    }
+
+    /**
+     * Get the percentage weighting for this grade component. This value
+     * specifically represents the ratio of potential points in this grade
+     * component to total points available across the entire course.
+     * 
+     * @return The percentage weighting of this grade component relative to the
+     *         rest of the course, represented as a float less than or equal to
+     *         1.0f.
+     */
+    public float getWeight() {
+        return mWeight;
+    }
+
+    /**
+     * Set the percentage weighting for this grade component. This value
+     * specifically represents the ratio of potential points in this grade
+     * component to total points available across the entire course.
+     * 
+     * @param weight
+     *            The percentage weighting of this grade component relative to
+     *            the rest of the course, represented as a float less than or
+     *            equal to 1.0f.
+     */
+    public void setWeight(float weight) {
+        mWeight = weight;
+    }
+
+    @Override
+    public void save() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ GradeCalculator/src/com/jetheis/android/grades/model/PointTotalGradeComponent.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2012 Jimmy Theis. Licensed under the MIT License:
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.jetheis.android.grades.model;
+
+/**
+ * A grade component that is scored based on a point total. The grade components
+ * weight relative to the rest of the course is based on how many additional
+ * points are available in the course.
+ */
+public class PointTotalGradeComponent extends GradeComponent {
+
+    private float mTotal;
+    private float mEarned;
+
+    /**
+     * Get the total number of points available to be earned for this grade
+     * component.
+     * 
+     * @return The total number of points available for this grade component.
+     */
+    public float getTotalPoints() {
+        return mTotal;
+    }
+
+    /**
+     * Set the total number of points available to be earned for this grade
+     * component.
+     * 
+     * @param total
+     *            The total number of points available for this grade component.
+     */
+    public void setTotalPoints(float total) {
+        mTotal = total;
+    }
+
+    /**
+     * Get the total number of points earned for this grade component.
+     * 
+     * @return The total number of points earned for this grade component.
+     */
+    public float getPointsEarned() {
+        return mEarned;
+    }
+
+    /**
+     * Set the total number of points earned for this grade component.
+     * 
+     * @param earned
+     *            The total number of points earned for this grade component.
+     */
+    public void setPointsEarned(float earned) {
+        mEarned = earned;
+    }
+
+    @Override
+    public void save() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
