a54c09b5924a6d47304350a43a9d73055961e0a1
Jimmy Theis <jimmy@jetheis.com>
2012-04-12 18:19:42 -0400

    Organize course list activity into its fragments
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- GradeCalculator/src/com/jetheis/android/grades/fragment/BuyFullVersionFragment.java
+++ GradeCalculator/src/com/jetheis/android/grades/fragment/BuyFullVersionFragment.java
@@ -22,8 +22,19 @@
 
 package com.jetheis.android.grades.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.actionbarsherlock.app.SherlockFragment;
+import com.jetheis.android.grades.R;
 
 public class BuyFullVersionFragment extends SherlockFragment {
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.buy_full_version_fragment, container, false);
+    }
 
 }
--- GradeCalculator/src/com/jetheis/android/grades/fragment/CourseListFragment.java
+++ GradeCalculator/src/com/jetheis/android/grades/fragment/CourseListFragment.java
@@ -22,8 +22,19 @@
 
 package com.jetheis.android.grades.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.actionbarsherlock.app.SherlockFragment;
+import com.jetheis.android.grades.R;
 
 public class CourseListFragment extends SherlockFragment {
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.course_list_fragment, container, false);
+    }
+    
 }
