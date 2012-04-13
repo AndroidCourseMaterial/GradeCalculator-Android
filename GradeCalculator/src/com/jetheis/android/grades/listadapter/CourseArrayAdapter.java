3acc6f87bd17e63d7e77db0e42e1c82d426224dc
Jimmy Theis <jimmy@jetheis.com>
2012-04-13 04:10:00 -0400

    Add missing files
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

---
+++ GradeCalculator/src/com/jetheis/android/grades/listadapter/CourseArrayAdapter.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2012 Jimmy Theis. Licensed under the MIT License:
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.jetheis.android.grades.listadapter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.jetheis.android.grades.R;
+import com.jetheis.android.grades.model.Course;
+
+public class CourseArrayAdapter extends ArrayAdapter<Course> {
+
+    private Context mContext;
+
+    public CourseArrayAdapter(Context context, List<Course> courses) {
+        super(context, R.layout.course_list_line_item, R.id.course_list_line_item_name_text_view, courses);
+
+        mContext = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View result = convertView;
+
+        if (result == null) {
+            LayoutInflater inflater = (LayoutInflater) mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            result = inflater.inflate(R.layout.course_list_line_item, null);
+        }
+        
+        Course currentCourse = getItem(position);
+
+        TextView nameTextView = (TextView) result
+                .findViewById(R.id.course_list_line_item_name_text_view);
+        nameTextView.setText(currentCourse.getName());
+
+        return result;
+    }
+
+}
