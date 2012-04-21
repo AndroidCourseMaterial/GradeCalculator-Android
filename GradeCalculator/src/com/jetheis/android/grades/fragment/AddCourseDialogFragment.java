c7faa9bd8635ab486736f1001b27c5c06d248742
Jimmy Theis <jimmy@jetheis.com>
2012-04-21 15:32:11 -0400

    Move enabled setting to code
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- GradeCalculator/src/com/jetheis/android/grades/fragment/AddCourseDialogFragment.java
+++ GradeCalculator/src/com/jetheis/android/grades/fragment/AddCourseDialogFragment.java
@@ -98,6 +98,7 @@ public class AddCourseDialogFragment extends SherlockDialogFragment {
             
         });
 
+        createButton.setEnabled(false);
         createButton.setOnClickListener(new OnClickListener() {
 
             @Override
